2d0496640615df23e0281038d158c7613ebb4ad4
Naman <namanddev7@gmail.com>
2021-08-11 16:36:54 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import speech_recognition as sr
+import base64
+from flask import Flask
+from flask import request, jsonify
+
+app = Flask(__name__)
+r = sr.Recognizer()
+
+
+@app.route("/api/stt",methods=["POST"])
+def hello_world():
+    body = request.get_json()
+    audio_base64 = body.get('audio')
+    audio_wav = base64.b64decode(audio_base64)
+    
+    wav_file = open("temp.wav", "wb")
+    wav_file.write(audio_wav)
+
+    with sr.AudioFile('temp.wav') as source:
+        try:
+            audio_text = r.listen(source)    
+            text = r.recognize_google(audio_text,language = "en-IN")
+            #print(text)
+            return jsonify({"text":text})
+        except:
+            return jsonify({"err": "something went wrong"})
+                
+app.run(debug=True)    
